86a4023d045d0c575c938796c3270642c1058f15
noman <nibfoysal@gmail.com>
2018-11-09 20:35:45 +0600

    Updates

---
+++ precache-manifest.e936ac86f43cba22c90a3a0ad1eff431.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee08484015d82bce9aab093b67e3b275",
+    "url": "/test_website_react/static/media/SL2.ee084840.png"
+  },
+  {
+    "revision": "1373f6bec8ee9bbc08a9",
+    "url": "/test_website_react/static/css/main.497576f9.chunk.css"
+  },
+  {
+    "revision": "5bc0d7d0299feecfddae",
+    "url": "/test_website_react/static/js/1.5bc0d7d0.chunk.js"
+  },
+  {
+    "revision": "23d0bc39f0392568f5c2",
+    "url": "/test_website_react/static/js/runtime~main.23d0bc39.js"
+  },
+  {
+    "revision": "dc0ad9b688705d58d03ecd5240296e5c",
+    "url": "/test_website_react/static/media/screen01.dc0ad9b6.png"
+  },
+  {
+    "revision": "91cd2834f46b44093e83fcd16469fbf7",
+    "url": "/test_website_react/static/media/screen02.91cd2834.png"
+  },
+  {
+    "revision": "1373f6bec8ee9bbc08a9",
+    "url": "/test_website_react/static/js/main.1373f6be.chunk.js"
+  },
+  {
+    "revision": "c5da527fdb7f87ca62568a44b79ab3b3",
+    "url": "/test_website_react/static/media/SL3.c5da527f.png"
+  },
+  {
+    "revision": "07f2cdd7fe3d10bace0d851e732c8db1",
+    "url": "/test_website_react/static/media/SL4.07f2cdd7.png"
+  },
+  {
+    "revision": "a2770f1f6accc5f74318257a62de9b50",
+    "url": "/test_website_react/static/media/logo_peep.a2770f1f.jpg"
+  },
+  {
+    "revision": "d1aae9b8a8f8587950f05a5659bb37c9",
+    "url": "/test_website_react/static/media/homebg.d1aae9b8.png"
+  },
+  {
+    "revision": "decd01096593882381a6ec0436dbbb30",
+    "url": "/test_website_react/index.html"
+  }
+];
